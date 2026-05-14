42158f79af4696a5676cc0ab26047727850437bd
Samuel <104882446+samuel-mekala@users.noreply.github.com>
2026-05-15 01:12:37 +0530

    Update server to use environment variable for port

--- server.py
+++ server.py
@@ -187,4 +187,6 @@ def predict():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
